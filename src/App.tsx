b7477b6ad9329e1704f2c2a3b59022635cf7b6cf
MorenaNobre <morenagnobre@gmail.com>
2021-08-23 06:33:40 -0300

    setting global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./Styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hellowww React! I missed you so much!!!</h1>
+      <GlobalStyle />
     </div>
   );
 }
